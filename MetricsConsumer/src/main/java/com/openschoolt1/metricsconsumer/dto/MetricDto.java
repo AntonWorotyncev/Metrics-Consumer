84393cc168867e2000e0cb62a376e160ab5e532a
Anton Worotyncev <avorotyntsev@gmail.com>
2024-05-19 17:43:14 +0300

    Рефакторинг для свайгера

--- MetricsConsumer/src/main/java/com/openschoolt1/metricsconsumer/dto/MetricDto.java
+++ MetricsConsumer/src/main/java/com/openschoolt1/metricsconsumer/dto/MetricDto.java
@@ -1,18 +1,26 @@
 package com.openschoolt1.metricsconsumer.dto;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
 
+@AllArgsConstructor
+@Getter
+@Setter
 @Schema(description = "Объект передачи данных, представляющий метрику")
-public record MetricDto(
+public class MetricDto{
         @Schema(description = "Уникальный идентификатор метрики", example = "1")
-        Long id,
+        Long id;
 
         @Schema(description = "Метка времени, когда метрика была записана", example = "1627845623000")
-        Long timestamp,
+        Long timestamp;
 
         @Schema(description = "Используемая память JVM в байтах", example = "1024")
-        Long jvmMemoryUsed,
+        Long jvmMemoryUsed;
 
         @Schema(description = "Максимально доступная память JVM в байтах", example = "2048")
-        Long jvmMemoryMax) {
+        Long jvmMemoryMax;
+
+
 }
